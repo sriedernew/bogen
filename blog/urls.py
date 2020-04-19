e8d071b85f1d963d6ecf482701ba2171489dbd86
sriedernew <susann.rieder@gmx.de>
2020-04-19 23:12:36 +0200

    sicherheit urls

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/addneu/', views.post_new, name='post_new'),
+    path('post/<int:pk>/bearbeiten/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/entfernen/', views.post_remove, name='post_remove'),
     path('kontakt', views.kontakt, name='kontakt'),
     path('home', views.home_list, name='home_list'),
     path('accounts/login/', views.forbidden, name='forbidden'),
